xrelkd/caracal

feat: initial commit

flake.nix
@@ -1,0 +1,104 @@
+{
+  description = "Caracal - download manager written in Rust Programming Language";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, fenix, crane }:
+    let
+      name = "caracal";
+      version = "0.1.0";
+    in
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              self.overlays.default
+              fenix.overlays.default
+            ];
+          };
+
+          rustToolchain = with fenix.packages.${system}; combine [
+            stable.rustc
+            stable.cargo
+            stable.clippy
+            stable.rust-src
+            stable.rust-std
+
+            default.rustfmt
+          ];
+
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = rustToolchain;
+            rustc = rustToolchain;
+          };
+
+          craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+          cargoArgs = [
+            "--workspace"
+            "--bins"
+            "--examples"
+            "--tests"
+            "--benches"
+            "--all-targets"
+          ];
+
+          unitTestArgs = [
+            "--workspace"
+          ];
+
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+          commonArgs = { inherit src; };
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        in
+        {
+          formatter = pkgs.treefmt;
+
+          devShells.default = pkgs.callPackage ./devshell {
+            inherit rustToolchain cargoArgs unitTestArgs;
+          };
+
+          packages = rec {
+            default = caracal;
+            caracal = pkgs.callPackage ./devshell/package.nix {
+              inherit name version rustPlatform;
+            };
+            container = pkgs.callPackage ./devshell/container.nix {
+              inherit name version caracal;
+            };
+          };
+
+          checks = {
+            format = pkgs.callPackage ./devshell/format.nix { };
+
+            rust-build = craneLib.cargoBuild (commonArgs // {
+              inherit cargoArtifacts;
+            });
+            rust-format = craneLib.cargoFmt { inherit src; };
+            rust-clippy = craneLib.cargoClippy (commonArgs // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = pkgs.lib.strings.concatMapStrings (x: x + " ") cargoArgs;
+            });
+            rust-nextest = craneLib.cargoNextest (commonArgs // {
+              inherit cargoArtifacts;
+              partitions = 1;
+              partitionType = "count";
+            });
+          };
+        })) // {
+      overlays.default = final: prev: { };
+    };
+}

xrelkd/catix

flake.nix
@@ -1,0 +1,104 @@
+{
+  description = "Catix - Nix binary cache proxy service";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, fenix, crane }:
+    let
+      name = "catix";
+      version = "0.1.0";
+    in
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              self.overlays.default
+              fenix.overlays.default
+            ];
+          };
+
+          rustToolchain = with fenix.packages.${system}; combine [
+            stable.rustc
+            stable.cargo
+            stable.clippy
+            stable.rust-src
+            stable.rust-std
+
+            default.rustfmt
+          ];
+
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = rustToolchain;
+            rustc = rustToolchain;
+          };
+
+          craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+          cargoArgs = [
+            "--workspace"
+            "--bins"
+            "--examples"
+            "--tests"
+            "--benches"
+            "--all-targets"
+          ];
+
+          unitTestArgs = [
+            "--workspace"
+          ];
+
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+          commonArgs = { inherit src; };
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        in
+        {
+          formatter = pkgs.treefmt;
+
+          devShells.default = pkgs.callPackage ./devshell {
+            inherit rustToolchain cargoArgs unitTestArgs;
+          };
+
+          packages = rec {
+            default = catix;
+            catix = pkgs.callPackage ./devshell/package.nix {
+              inherit name version rustPlatform;
+            };
+            container = pkgs.callPackage ./devshell/container.nix {
+              inherit name version catix;
+            };
+          };
+
+          checks = {
+            format = pkgs.callPackage ./devshell/format.nix { };
+
+            rust-build = craneLib.cargoBuild (commonArgs // {
+              inherit cargoArtifacts;
+            });
+            rust-format = craneLib.cargoFmt { inherit src; };
+            rust-clippy = craneLib.cargoClippy (commonArgs // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = pkgs.lib.strings.concatMapStrings (x: x + " ") cargoArgs;
+            });
+            rust-nextest = craneLib.cargoNextest (commonArgs // {
+              inherit cargoArtifacts;
+              partitions = 1;
+              partitionType = "count";
+            });
+          };
+        })) // {
+      overlays.default = final: prev: { };
+    };
+}